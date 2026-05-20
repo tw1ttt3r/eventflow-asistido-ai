720d3cf09d227241171df186a4528984147bf084
tw1ttt3r <tw1ttt3r@gmail.com>
2026-05-20 10:55:09 -0600

    feat: version configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,10 @@
 import type { Environment } from './environment.model';
+import { APP_VERSION } from '../version';
 
 export const environment: Environment = {
   production: true,
   apiUrl: 'https://api.eventflow.example',
   appName: 'Eventflow Asistido',
+  version: APP_VERSION,
+
 };
