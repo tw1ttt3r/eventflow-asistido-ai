6ed7fc5849ecb48f8b31ce8deaf561ded183af80
tw1ttt3r <tw1ttt3r@gmail.com>
2026-06-28 14:35:25 -0600

    feat: declarated paths

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,13 @@
-import type { Environment } from './environment.model';
+import type { Environment } from '@env/environment.model';
 import { APP_VERSION } from '../version';
+import { APP_VERSION } from '@version';
 
 export const environment: Environment = {
   production: false,
   apiUrl: '',
   appName: '',
   version: APP_VERSION,
+
 
 
 
